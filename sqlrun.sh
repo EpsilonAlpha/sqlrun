5a56508f51a859b991e94b0745882f32ac66f861
EpsilonAlpha <flirtmaster@me.com>
2019-04-02 04:29:17 +0200

    Update sqlrun.sh
    - capsule Inputs into Quotes

--- sqlrun.sh
+++ sqlrun.sh
@@ -2,9 +2,9 @@
 # Reason for this Script is it, to provide a common Interface for all Database Operations to an MySQL Server
 
 # Given Parameters
-targetdatabase=$1
-configfilepath=$2
-sqloptions=$3
+targetdatabase="$1"
+configfilepath="$2"
+sqloptions="$3"
 sqlparameters="--defaults-extra-file=$configfilepath $sqloptions $targetdatabase"
 sqlcommand="$4"
 
